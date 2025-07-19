a74d3c00b7c68ecb18f7ccce6c6c1f27b3034fce
Prashant Solanki <prashantsolanki2025@gmail.com>
2025-07-19 19:11:07 +0530

    Residences Page added

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -157,6 +157,17 @@ export const api = createApi({
       query: (cognitoId) => `tenants/${cognitoId}`,
       providesTags: (result) => [{ type: "Tenants", id: result?.id }],
     }),
+
+    getCurrentResidences: build.query<Property[], string>({
+      query: (cognitoId) => `tenants/${cognitoId}/current-residences`,
+      providesTags: (result) =>
+        result
+          ? [
+              ...result.map(({ id }) => ({ type: "Properties" as const, id })),
+              { type: "Properties", id: "LIST" },
+            ]
+          : [{ type: "Properties", id: "LIST" }],
+    }),
   }),
 });
 
@@ -169,4 +180,5 @@ export const {
   useAddFavoritePropertyMutation,
   useRemoveFavoritePropertyMutation,
   useGetTenantQuery,
+  useGetCurrentResidencesQuery,
 } = api;
