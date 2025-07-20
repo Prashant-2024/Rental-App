5f9940ee296cd339979dea63e991b6f819116f01
Prashant Solanki <prashantsolanki2025@gmail.com>
2025-07-20 16:19:38 +0530

    S3 Fixes

--- client/next.config.ts
+++ client/next.config.ts
@@ -5,6 +5,12 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "example.com", port: "", pathname: "/**" },
+      {
+        protocol: "https",
+        hostname: "*.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
