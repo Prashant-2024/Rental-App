2f044ebe4f3064e1be9e04e172cfe4cda993363d
Prashant Solanki <prashantsolanki2025@gmail.com>
2025-07-20 16:45:27 +0530

    Fixes

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -74,7 +74,7 @@ const Navbar = () => {
                 router.push(
                   authUser.userRole?.toLowerCase() === "manager"
                     ? "/managers/newproperty"
-                    : "search"
+                    : "/search"
                 );
               }}
             >
